d40f337412cd3e7baa0e03d7400cde95a7adbc26
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2020-04-07 02:10:02 +0200

    multi-version 1.0
    initial      ddd

---
+++ OddEvenBotApp_multi/18-03-23-BinaryBetBotApp_multi/OddEvenBotApp/Model/AuthorizeModel.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddEvenBotApp.Model
+{
+    public class AuthorizeModel
+    {
+        public Authorize authorize { get; set; }
+        public string msg_type { get; set; }
+        public class Authorize {
+            public Account_list[] account_list { get; set; }
+            public string balance { get; set; }
+            public string country { get; set; }
+            public string currency { get; set; }
+            public string email { get; set; }
+            public string fullname { get; set; }
+            public int is_virtual { get; set; }
+            public string landing_company_fullname { get; set; }
+            public string landing_company_name { get; set; }
+            public Local_currencies local_currencies { get; set; }
+            public string loginid { get; set; }
+            public string[] scopes { get; set; }
+            public string[] upgradeable_landing_companies { get; set; }
+            public class Account_list
+            {
+                public string currency { get; set; }
+                public int is_disabled { get; set; }
+                public int is_virtual { get; set; }
+                public string landing_company_name { get; set; }
+                public string loginid { get; set; }
+            }
+            public class Local_currencies
+            {
+                public ROL ROL { get; set; }
+            }
+            public class ROL
+            {
+                public int fractional_digits { get; set; }
+            }
+        }
+    }
+}
